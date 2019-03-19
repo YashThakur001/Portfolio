bb493cfdf06d1624d87db9d31f696e492a865579
ythakur <yash.thakur@trader.ca>
2019-03-19 12:03:11 -0400

    update script

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+//On click of the button show the menu to navigate
 document.querySelector("button.navbar-toggler.collapsed").addEventListener("click", show);
-	function show(){
+//On click of menu list hide the menu
+document.querySelector('ul.navbar-nav').addEventListener("click", hide);
+	
+//Show the menu
+function show(){
 	var element = document.querySelector('div#navbarSupportedContent');
-	if (element.style.display === "block") {
-	element.style.display = "none";
-	} else {
 	element.style.display = "block";
-	}
+}
+
+//Hide the menu after chosing one option
+function hide(){
+	var element1 = document.querySelector('div#navbarSupportedContent');
+	element1.style.display = "none";
 }
