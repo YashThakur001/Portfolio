2940c6a6d3dd969647426e9bbeec1efbe18b9b78
ythakur <yash.thakur@trader.ca>
2019-03-19 11:44:23 -0400

    update script

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 document.querySelector("button.navbar-toggler.collapsed").addEventListener("click", show);
 	function show(){
 	var element = document.querySelector('div#navbarSupportedContent');
-	if (element.style.display === "none") {
-	element.style.display = "block";
-	} else {
+	if (element.style.display === "block") {
 	element.style.display = "none";
+	} else {
+	element.style.display = "block";
 	}
 }
